58795da149118de3257301abc6ba03c08d005de4
Bradley Morrison <45197952+brad-morrison@users.noreply.github.com>
2022-10-24 10:49:47 +0100

    Changed image from absolute positioning to relative
    Got image working, now attempting the text body.

--- src/components/objects/PostCard.js
+++ src/components/objects/PostCard.js
@@ -20,54 +20,28 @@ function PostCard(props) {
 export default PostCard
 
 const CardWrapper = styled.div`
-  position: relative;
-  //display: grid;
-  //grid-template-columns: auto;
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  overflow: hidden;
-
+  width: 100%;
+  display: grid;
+  grid-template-rows: 70% auto;
   background: #f4f4ef;
   border-radius: 22px;
+  overflow: hidden;
 `
 
-const CardImageWrapper = styled.div`
-  position: relative;
-`
+const CardImageWrapper = styled.div``
 
 const CardImage = styled.img`
-  & {
-    //width: 100%;
-    height: 100%;
-    max-width: 100%;
-
-    //padding-bottom: 56%;
-  }
-  &:before {
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    object-fit: cover;
-    object-position: center;
-  }
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
 `
 
-const CardText = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  padding: 0 38px;
-  min-height: 10rem;
-  gap: 20px;
-  margin: 20px 0;
-`
+const CardText = styled.div``
 
 const CardTitle = styled.p`
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  //font-family: "SF Pro Display";
+
   font-style: normal;
   font-weight: 800;
   font-size: 26px;
@@ -79,7 +53,6 @@ const CardTitle = styled.p`
 const CardSubTitle = styled.p`
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  //font-family: "SF Pro Display";
   font-style: normal;
   font-weight: 400;
   font-size: 22px;
