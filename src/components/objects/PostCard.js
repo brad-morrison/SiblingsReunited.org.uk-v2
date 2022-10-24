11b33303b9d8e501476a8e01e01cbd73b8af9b74
Bradley Morrison <45197952+brad-morrison@users.noreply.github.com>
2022-10-24 11:07:58 +0100

    I have made it work, although the image can sometimes scale wrongly when dragging the window smaller

--- src/components/objects/PostCard.js
+++ src/components/objects/PostCard.js
@@ -22,10 +22,11 @@ export default PostCard
 const CardWrapper = styled.div`
   width: 100%;
   display: grid;
-  grid-template-rows: 70% auto;
+  grid-template-rows: 1fr auto;
   background: #f4f4ef;
   border-radius: 22px;
   overflow: hidden;
+  height: auto;
 `
 
 const CardImageWrapper = styled.div``
@@ -36,7 +37,12 @@ const CardImage = styled.img`
   height: 100%;
 `
 
-const CardText = styled.div``
+const CardText = styled.div`
+  display: grid;
+  gap: 10px;
+  padding: 20px;
+  height: auto;
+`
 
 const CardTitle = styled.p`
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
