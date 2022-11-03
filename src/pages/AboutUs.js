3b1a47b72dbdb17cc7fca55ea0f2f654246e3b8a
Bradley Morrison <45197952+brad-morrison@users.noreply.github.com>
2022-11-03 23:11:34 +0000

    changes

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+function AboutUsPage() {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>About Us</h1>
+    </Layout>
+  )
+}
+
+export default AboutUsPage
