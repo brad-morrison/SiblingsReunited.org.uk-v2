3743f3f350e5ab2671b1517d03892f5779973d67
Bradley Morrison <45197952+brad-morrison@users.noreply.github.com>
2022-11-18 00:34:31 +0000

    Many changes

---
+++ src/components/objects/TeamCard.js
@@ -0,0 +1,46 @@
+import React from "react"
+import styled from "styled-components"
+import { themes } from "../styles/ColorStyles"
+import { BodyIntro, Caption, Caption2, SmallText } from "../styles/TextStyles"
+
+export default function TeamCard(props) {
+  return (
+    <Wrapper>
+      <Image img={props.image}></Image>
+      <Details>
+        <MemberName>{props.firstName}</MemberName>
+        <MemberRole>{props.role}</MemberRole>
+      </Details>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  width: fit-content;
+  display: grid;
+  box-shadow: 0px 7px 22px rgba(0, 0, 0, 0.1);
+`
+
+const Image = styled.img`
+  width: 200px;
+  height: 200px;
+  background: url(${props => props.img});
+  background-size: cover;
+`
+
+const Details = styled.div`
+  display: grid;
+  padding: 12px 16px;
+  gap: 6px;
+  background-color: ${themes.secondaryBackground};
+`
+
+const MemberName = styled(Caption2)`
+  font-size: 16px;
+`
+
+const MemberRole = styled(SmallText)`
+  opacity: 60%;
+  font-style: italic;
+  font-size: 14px;
+`
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,15 +1,103 @@
 import * as React from "react"
+import styled from "styled-components"
 import Layout from "../components/layout"
+import TeamCard from "../components/objects/TeamCard"
 import TopicPageTitle from "../components/sections/TopicPageTitle"
 import Seo from "../components/seo"
+import { breaks } from "../components/styles/BreakStyles"
+import { BodyMain, H2, H3 } from "../components/styles/TextStyles"
+import { teamMembers } from "../data/TeamMembers"
 
-function AboutUsPage() {
+export default function AboutUsPage() {
   return (
     <Layout>
       <Seo title="AboutUs" />
       <TopicPageTitle title="About Us" img="/images/volunteer.jpg" />
+      <Block>
+        <SubHeading>Who we are</SubHeading>
+        <Text>
+          A major review of Scotland's care system has said the pain of brothers
+          and sisters being separated often has "profound and lifelong
+          consequences."
+        </Text>
+        <Text>
+          Follow us on social media to keep up with the latest news or find out
+          more. We love letting everyone know what our sibling groups have been
+          up to!
+        </Text>
+      </Block>
+      <Block>
+        <SubHeading>What we do</SubHeading>
+        <Text>
+          It was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum.
+        </Text>
+        <WideImage></WideImage>
+        <Text>
+          Follow us on social media to keep up with the latest news or find out
+          more. We love letting everyone know what our sibling groups have been
+          up to!
+        </Text>
+      </Block>
+      <Block>
+        <SubHeading>Meet the Siblings Reunited team</SubHeading>
+        <Text>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s.
+        </Text>
+        <Team>
+          {teamMembers.map((member, index) => (
+            <TeamCard
+              key={index}
+              firstName={member.firstName}
+              lastName={member.lastName}
+              role={member.role}
+              image={member.image}
+            ></TeamCard>
+          ))}
+        </Team>
+      </Block>
     </Layout>
   )
 }
 
-export default AboutUsPage
+const Block = styled.div`
+  max-width: 1200px;
+  display: grid;
+  gap: 40px;
+  justify-content: center;
+  margin: auto;
+  padding: 30px;
+`
+
+const SubHeading = styled(H3)``
+
+const Text = styled(BodyMain)``
+
+const WideImage = styled.img`
+  width: 100%;
+  height: 300px;
+  background: url("/images/hero2.jpeg");
+  background-size: cover;
+  background-position: 0% 70%;
+  margin: 10px 0px;
+`
+
+const Team = styled.div`
+  display: grid;
+  justify-content: space-evenly;
+  gap: 30px;
+  grid-template-columns: auto auto auto auto;
+  margin: 50px 0px;
+
+  @media (max-width: ${breaks.tablet}) {
+    grid-template-columns: auto auto auto;
+  }
+
+  @media (max-width: 770px) {
+    grid-template-columns: auto auto;
+  }
+`
